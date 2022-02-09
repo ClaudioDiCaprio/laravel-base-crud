37d23569189f634e466017ad87027de034c42b51
Claudio <claudiodicaprio89@gmail.com>
2022-02-09 18:29:19 +0100

    bootstrap_tabled_comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.base')
 
 @section('pageContent')
-    <ul>
-        @foreach ($comics as $comic)
-            <li>{{$comic->name}}</li>
-        @endforeach
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+                <th scope="col">price</th>
+                <th scope="col">sale_data</th>
+              </tr>
+            </thead>
+            <tbody>
+            @foreach ($comics as $comic)
+              <tr>
+                <th>{{$comic->id}}</th>
+                <td>{{$comic->name}}</td>
+                <td>{{$comic->price}}</td>
+                <td>{{$comic->sale_data}}</td>
+              </tr>
+            @endforeach
+            </tbody>
+        </table>
     </ul>
 @endsection
